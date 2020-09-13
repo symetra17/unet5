893c7cd23607498253ae76fd5f1da8d94b387a0b
unknown <chikman428@gmail.com>
2020-09-14 06:30:49 +0800

    ...

--- guicfg.py
+++ guicfg.py
@@ -9,9 +9,9 @@ cls_list = ['Farmland','Trees','Vehicles','Squatter','Solar']
 def get(cls_name):
     if cls_name == "Vehicles":
         class cls_cfg:
-            my_size = 576
-            down_scale = 2
-            bands = 5
+            my_size = 512
+            down_scale = 1
+            bands = 4
             cls_sub_list = OrderedDict({'Vehicles':1})
     elif cls_name == "Trees":
         class cls_cfg:
