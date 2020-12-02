b54d1a62bc699a6275327abb953174bf8a6ee963
unknown <chikman428@gmail.com>
2020-12-03 07:34:37 +0800

    speeded up rotation and reduce memory requirement

--- json_conv.py
+++ json_conv.py
@@ -12,7 +12,7 @@ import geotiff
 import multiprocessing as mp
 import random
 import time
-import ray
+from itertools import repeat
 
 
 
@@ -30,46 +30,75 @@ def get_rand_angle():
     return angle
 
 def read_img_anno(inplist, im_file_name, cls_name):
-    img = geotiff.imread(im_file_name)  # 5-channel array included NIR and DSM.
+    
+    img = geotiff.imread(im_file_name)  # 5-channel array included NIR and DSM.    
     img = np.nan_to_num(img)
     anno_im = np.zeros((img.shape[0], img.shape[1]), dtype=np.uint8)
-    ntotal_out = 0
+    
     cls_sub_list = cfg.get(cls_name).cls_sub_list
+
     for o in inplist:
         pts = np.array(o['points']).astype(np.int32)
         try:
             classid = cls_sub_list[o['label']]
         except:
             classid = 0
-            print('Unexpected class label: ', o['label'])
+            if o['label'] != 'discard':
+                print('Unexpected class label: ', o['label'])
         cv2.fillPoly(anno_im, [pts], (classid))
-        ntotal_out += 1
-    return img, anno_im, ntotal_out
+    
+    for o in inplist:
+        pts = np.array(o['points']).astype(np.int32)
+        if o['label'] == 'discard':
+            cv2.fillPoly(anno_im, [pts], (255))
+
+    return img, anno_im
 
 
-@ray.remote
-def skimage_rotate(im_file_name, img, n, angle):
-    img1 = skimage.transform.rotate(img.copy(), angle, resize=True, 
+def skimage_rotate(im_file_name, img, n, angle, dn_scale):
+    img_ls = []
+    n_layer = img.shape[2]
+    for k in range(n_layer):
+        img1 = skimage.transform.rotate(img[:,:,k], angle, resize=True, 
                         mode='constant', cval=-1.0, preserve_range=True)
-    img1 = img1.astype(np.half)
+        if dn_scale != 1:
+            img1 = cv2.resize(img1, None, fx=1/dn_scale, fy=1/dn_scale, 
+                interpolation=cv2.INTER_AREA)
+        img_ls.append(img1)
+
+    h = img_ls[0].shape[0]
+    w = img_ls[0].shape[1]
+
+    out_img = np.zeros( (h,w,img.shape[2]), dtype=img_ls[0].dtype )
+    for k, layer in enumerate(img_ls):
+        out_img[:,:,k] = layer.copy()
+
+    img1 = out_img.astype(np.half)
     np.save(os.path.splitext(im_file_name)[0] + '_im_rot_%d'%n, img1)     # save it for use next time
 
-@ray.remote
-def skimage_rotate_annot(im_file_name, anno_im, n, angle):
+
+def skimage_rotate_annot(im_file_name, anno_im, n, angle, dn_scale):
     anno_im1 = skimage.transform.rotate(anno_im, angle, resize=True, 
             mode='constant', cval=0, preserve_range=True)
+    if dn_scale != 1:
+        anno_im1 = cv2.resize(anno_im1, None, fx=1/dn_scale, fy=1/dn_scale, 
+            interpolation=cv2.INTER_AREA)
     anno_im1 = anno_im1.astype(np.half)
     np.save(os.path.splitext(im_file_name)[0] + '_anno_rot_%d'%n, anno_im1)
 
-@ray.remote
+
 def filter_save_training_patch(img, anno_im, ystart, yend, xstart,xend, 
-        my_size, down_scale, 
+        my_size, 
         outname, a_or_b, cls_cfg_discard_empty):
 
     sub_im = img[ystart:yend, xstart:xend, :]
     sub_im = sub_im.astype(np.float32)
 
     sub_anno = anno_im[ystart:yend, xstart:xend]
+
+    if sub_anno.max() ==255:   # if any pixel is marked as discard in json file
+        return
+
     # Pad image to 1024 divisible
     sub_img_h, sub_img_w = sub_im.shape[0:2]
     if sub_img_h < my_size:
@@ -89,17 +118,15 @@ def filter_save_training_patch(img, anno_im, ystart, yend, xstart,xend,
     assert sub_anno.shape[0] == my_size
     assert sub_anno.shape[1] == my_size
 
-    if down_scale != 1:
-        sub_im = cv2.resize(sub_im, None, fx=1/down_scale, fy=1/down_scale,
-                interpolation=cv2.INTER_AREA)
-        sub_anno = cv2.resize(sub_anno, None, fx=1/down_scale, fy=1/down_scale,
-                interpolation=0)
     # Some images contain large blank black or white area, and these 
     # area should not be included in training.
-    if int(np.percentile(sub_im, 40)) < 0:      # too many rotation out of boundary area
+
+    # too many pixel has rotated out of boundary area
+    idx = np.where(sub_im[:,:,0] < 0)
+    empty_ratio = len(idx[0]) / (my_size*my_size)
+    if empty_ratio > 0.4:
         return
-    if int(np.percentile(sub_im, 60)) >= 255:   # too many white empty area
-        return
+
     if int(np.percentile(sub_anno, 99)) == 0:
         v = random.random()
         if v < cls_cfg_discard_empty:  # keep some of background picture
@@ -110,29 +137,26 @@ def filter_save_training_patch(img, anno_im, ystart, yend, xstart,xend,
     sub_im[:,:,0] = sub_im_0
 
 
-
     if sub_im.shape[2] > 1:
         sub_im_1 = sub_im[:,:,1].copy()
         sub_im_1[sub_im_1<0] = 128.0
         sub_im[:,:,1] = sub_im_1
-
-
 
     if sub_im.shape[2] > 2:
         sub_im_2 = sub_im[:,:,2].copy()
         sub_im_2[sub_im_2<0] = 128.0
         sub_im[:,:,2] = sub_im_2
 
-
-
     if sub_im.shape[2] > 3:
         sub_im_3 = sub_im[:,:,3].copy()
         sub_im_3[sub_im_3<0] = 128.0
         sub_im[:,:,3] = sub_im_3
+
     if sub_im.shape[2] > 4:
         sub_im_4 = sub_im[:,:,4].copy()
         sub_im_4[sub_im_4<0] = 0
         sub_im[:,:,4] = sub_im_4
+
     outpath = path_insert_folde(outname, 'slice'+a_or_b)
     outpath = path_insert_folde(outpath, 'annotation')
     cv2.imwrite(outpath + '.png', sub_anno.astype(np.float32))
@@ -140,13 +164,13 @@ def filter_save_training_patch(img, anno_im, ystart, yend, xstart,xend,
     outpath = path_insert_folde(outpath, 'image')
     geotiff.imwrite(outpath, sub_im.astype(np.float32))
 
-@ray.remote
-def load_img_npy(fname):
-    img = np.load(fname)
-    return img
+    # RGB jpg for manual inspection
+    geotiff.imwrite(os.path.splitext(outpath)[0]+'.jpg', 
+            sub_im[:,:,0:3].astype(np.uint8))
+
 
 def split_label(inplist, im_file_name, cls_name, a_or_b):
-
+    
     reload(cfg)
     cls_cfg = cfg.get(cls_name)
     try:
@@ -156,14 +180,19 @@ def split_label(inplist, im_file_name, cls_name, a_or_b):
 
     my_size = cls_cfg.my_size * down_scale
     random.seed()
-    augmen_x = int(cfg.augm_translate) * random.randint(0, my_size//2)
-    augmen_y = int(cfg.augm_translate) * random.randint(0, my_size//2)
         
     if not cfg.augm_rotation:
-        img, anno_im, _ = read_img_anno(inplist, im_file_name, cls_name)
+
+        img, anno_im = read_img_anno(inplist, im_file_name, cls_name)
+        img = cv2.resize(img, None, fx=1/down_scale, fy=1/down_scale, 
+                interpolation=cv2.INTER_AREA)
+        anno_im = cv2.resize(anno_im, None, fx=1/down_scale, fy=1/down_scale, 
+                interpolation=0)
+
     else:
-        anglels = [-30, -15, 0, 15, 30]
-        #print('loading rot backup ', os.path.split(im_file_name)[-1])
+
+        anglels = [-30, -20, -10, 0, 10, 20, 30]
+
         angle_idx = random.randint(0, len(anglels)-1)
         fname_im = os.path.splitext(im_file_name)[0] + '_im_rot_%d'%angle_idx
         fname_anno = os.path.splitext(im_file_name)[0] + '_anno_rot_%d'%angle_idx
@@ -171,99 +200,68 @@ def split_label(inplist, im_file_name, cls_name, a_or_b):
             pass
         else:
             t0 = time.time()
-            print('Generating rot img: ', os.path.split(im_file_name)[-1])
-            img, anno_im, _ = read_img_anno(inplist, im_file_name, cls_name)
-
-            pid_ls = []
-            shmem_id = ray.put(img)
+            print('Generating rotated img: ', os.path.split(im_file_name)[-1])
+            img, anno_im = read_img_anno(inplist, im_file_name, cls_name)
+            
             for n, angle in enumerate(anglels):
-                pid_ls.append(skimage_rotate.remote(im_file_name, shmem_id, n, angle))
-            ray.get(pid_ls)
+                print('rotating ', angle, ' deg')
+                skimage_rotate(im_file_name, img, n, angle, down_scale)
 
-            print('rotating annotation')
-            pid_ls = []
-            shmem_id = ray.put(anno_im)
+            print('Rotating annotation')
             for n, angle in enumerate(anglels):
-                pid_ls.append(skimage_rotate_annot.remote(im_file_name, shmem_id, n, angle))
-            ray.get(pid_ls)
+                skimage_rotate_annot(im_file_name, anno_im, n, angle, down_scale)
 
-            #for n, angle in enumerate(anglels):
-            #    #img1 = skimage.transform.rotate(img, angle, resize=True, 
-            #    #        mode='constant', cval=-1.0, preserve_range=True)
-            #    #img1 = img1.astype(np.half)
-            #    #np.save(os.path.splitext(im_file_name)[0] + '_im_rot_%d'%n, img1)     # save it for use next time
-            #    #del img1
-            #
-            #    anno_im1 = skimage.transform.rotate(anno_im, angle, resize=True, 
-            #            mode='constant', cval=0, preserve_range=True)
-            #    anno_im1 = anno_im1.astype(np.half)
-            #    np.save(os.path.splitext(im_file_name)[0] + '_anno_rot_%d'%n, anno_im1)
             t1 = time.time()
             print('rotation augm time:', int(t1-t0), 'sec')
 
-        t0=time.time()
-        pid = load_img_npy.remote(fname_im + '.npy')
+        img     = np.load(fname_im + '.npy')
         anno_im = np.load(fname_anno + '.npy').astype(np.uint8)
-        img = ray.get([pid])[0]
-        t1=time.time()
-        print("load npy file  %.1f sec"%(t1-t0))
 
-    #print('src image check: ', img.dtype, img.max(), img.min(), np.isinf(img), np.isnan(img))
-
+    my_size = cls_cfg.my_size
+    augm_x = int(cfg.augm_translate) * random.randint(0, my_size-1)
+    augm_y = int(cfg.augm_translate) * random.randint(0, my_size-1)
     
-    pid_ls = []
-
-    t0 = time.time()
-
-    im_id = ray.put(img)
-    anno_id = ray.put(anno_im)
-
     for n in range(1+int(img.shape[1]/my_size)):
         for m in range(1+int(img.shape[0]/my_size)):
     
-            xstart = n * my_size + augmen_x
+            xstart = n * my_size + augm_x
             xend   = xstart + my_size
             if xstart >= img.shape[1]:
                 continue
             if xend > img.shape[1]:
                 xend = img.shape[1]
 
-            ystart = m * my_size + augmen_y
+            ystart = m * my_size + augm_y
             yend   = ystart + my_size
             if ystart >= img.shape[0]:
                 continue
             if yend > img.shape[0]:
                 yend = img.shape[0]
             
-            outname=remove_ext(im_file_name) + '_W_%d_H_%d_X_%d_%d_Y_%d_%d'%(
+            outname = remove_ext(im_file_name) + '_W_%d_H_%d_X_%d_%d_Y_%d_%d'%(
                 img.shape[1],img.shape[0],xstart,xend,ystart,yend)
 
-            pid = filter_save_training_patch.remote(im_id, anno_id, ystart, yend, xstart,xend, 
-                my_size, down_scale, outname, a_or_b, cls_cfg.discard_empty)
-            pid_ls.append(pid)
-            
-    ray.get(pid_ls)
-    t1 = time.time()
-    print("cropping %.1f"%(t1-t0))
+            filter_save_training_patch(img, anno_im, ystart, yend, xstart,xend, 
+                my_size, outname, a_or_b, cls_cfg.discard_empty)
 
-    
-def mp_split(files, cls_name, a_or_b):
-    for fname in files:
-        inp_json = remove_ext(fname) + '.json'
-        if not os.path.exists(inp_json):
-            print('Error: Could not find label json file for ', fname)
-            quit()
-        else:
-            fid = open(inp_json, 'r')
-            str1 = fid.read()
-            fid.close()
-            y = json.loads(str1)
-            nobj = len(y['shapes'])
-            objects_list = y['shapes']
-            split_label(objects_list, fname, cls_name, a_or_b)
+            
+
+def mp_split(fname, cls_name, a_or_b):
+
+    inp_json = remove_ext(fname) + '.json'
+    if not os.path.exists(inp_json):
+        print('Error: Could not find label json file for ', fname)
+        return
+
+    fid = open(inp_json, 'r')
+    str1 = fid.read()
+    fid.close()
+    y = json.loads(str1)
+    nobj = len(y['shapes'])
+    objects_list = y['shapes']
+    split_label(objects_list, fname, cls_name, a_or_b)
 
 def xxx(foder, cls_name, a_or_b):
-
 
     def clean_folder(folder):
         try:
@@ -298,35 +296,14 @@ def xxx(foder, cls_name, a_or_b):
     create_clean_folder(ann_path)
     create_clean_folder(im_path)
 
-    using_ray = True
-
-    if not using_ray:
-        nf = len(files)
-        n_thread = mp.cpu_count() - 2
-        if n_thread<=0:
-            n_thread = 1
-        if n_thread > nf:
-            n_thread = nf
-        n_thread_max = 4
-        if n_thread > n_thread_max:
-            n_thread = n_thread_max
-        file_groups = []
-        for n in range((n_thread-1)):
-            file_groups.append(files[n*nf//n_thread: (n+1)*nf//n_thread])
-        file_groups.append(files[(n_thread-1)*nf//n_thread: ])
-        proc_group = []
-        for n in range(n_thread):
-            proc_group.append(mp.Process(target=mp_split, args=(file_groups[n],cls_name,a_or_b,)))
-        for n in range(n_thread):
-            proc_group[n].start()
-        for n in range(n_thread):
-            proc_group[n].join()
-    else:
-        mp_split(files,cls_name,a_or_b)
+    pool = mp.Pool(processes=8)
+    for n in range(2):
+        args = zip( files, repeat(cls_name), repeat(a_or_b) )
+        pool.starmap(mp_split, args)
 
 
-def illustrate_mask(im_file_name, class_dict):
 
+def illustrate_dsm_mask(im_file_name, class_dict):
     inp_json=remove_ext(im_file_name) + '.json'
     if not os.path.exists(inp_json):
         return
@@ -337,22 +314,14 @@ def illustrate_mask(im_file_name, class_dict):
         y = json.loads(str1)
         objects_list = y['shapes']
 
-    img = cv2.imread(im_file_name)
-    img_ol = img.copy()
-
-    img_h, img_w = img.shape[0:2]
-    anno_im = np.zeros_like(img)
-    label_only_im = np.zeros_like(img)
-
+    img = geotiff.imread(im_file_name)
+    img_dsm = img[:,:,4].copy()
+    img_dsm = cv2.cvtColor(img_dsm, cv2.COLOR_GRAY2BGR)
+    img_dsm_ol = img_dsm.copy()
     fill_colour = [(255,255,0)]
-
     for o in objects_list:
         pts = np.array(o['points']).astype(np.int32)
-        if o['label'] == 'discard':
-            classid = 0
-        else:
-            classid = class_dict[o['label']]
-
+        classid = 1   #class_dict[o['label']]
         if classid==0:
             fill_colour = (0,0,0)
         elif classid==1:
@@ -363,15 +332,66 @@ def illustrate_mask(im_file_name, class_dict):
             fill_colour = (255,0,0)
         elif classid==4:
             fill_colour = (255,255,0)
-        cv2.fillPoly(img_ol, [pts], fill_colour)
-        cv2.fillPoly(label_only_im, [pts], (255,255,255))
+        cv2.fillPoly(img_dsm_ol, [pts], (0,0,255) )
+        img_dsm_ol = cv2.polylines( img_dsm_ol, [pts], True, (255,0,0), 10 )
 
-    cv2.imwrite(remove_ext(im_file_name)+'_label_no_bkg.bmp', label_only_im)
+    anno_im_dsm = img_dsm * 0.7 + img_dsm_ol * 0.3
+    geotiff.imwrite(remove_ext(im_file_name)+'_label_dsm.jpg', anno_im_dsm.astype(np.uint8))
 
-    anno_im = img/2 + img_ol/2
-    cv2.imwrite(remove_ext(im_file_name)+'_label.jpg', anno_im)
 
-    
+def illustrate_mask(im_file_name, json_fname):
+
+    fid = open(json_fname, 'r')
+    str1 = fid.read()
+    fid.close()
+    y = json.loads(str1)
+    objects_list = y['shapes']
+
+    img = geotiff.imread(im_file_name)
+    img = img[:,:,0:3]
+    img_ol = img.copy()
+    img_ol = img_ol.astype(np.uint8)
+    img_h, img_w = img.shape[0:2]
+    label_only_im = np.zeros_like(img)
+    fill_colour = [(255,255,0)]
+    for o in objects_list:
+        pts = np.array(o['points']).astype(np.int32) //4
+        if o['label'] == 'discard':
+            cv2.fillPoly(img_ol, [pts], (128,128,128) )
+        else:
+            classid = 1
+            if classid==0:
+                fill_colour = (0,0,0)
+            elif classid==1:
+                fill_colour = (0,255,255)   # (R,G,B)   
+            img_ol = cv2.polylines( img_ol, [pts], True, (0,255,0), 1 )
+        
+    anno_im = img * 0.3 + img_ol * 0.7
+    geotiff.imwrite(remove_ext(im_file_name)+'_label.jpg', anno_im.astype(np.uint8))
+
+def illustrate_mask_bw(json_fname):
+
+    fid = open(json_fname, 'r')
+    str1 = fid.read()
+    fid.close()
+    y = json.loads(str1)
+    objects_list = y['shapes']
+    imageHeight = int(y['imageHeight'])
+    imageWidth = int(y['imageWidth'])
+
+    img_ol = np.zeros((imageHeight, imageWidth), dtype=np.uint8)
+    for o in objects_list:
+        pts = np.array(o['points']).astype(np.int32)
+        cv2.fillPoly(img_ol, [pts], (255) )
+
+    for o in objects_list:
+        pts = np.array(o['points']).astype(np.int32)
+        if o['label'] == 'discard':
+            cv2.fillPoly(img_ol, [pts], (128) )
+            
+    cv2.imwrite('img_ol.png',img_ol)
+    return img_ol
+
 def get_class_name(img_files):
     nobj = 0
     cls_dict = OrderedDict()
@@ -391,51 +411,54 @@ def get_class_name(img_files):
                 cls_dict[cls_name] = True
     return list(cls_dict.keys())
 
-if __name__=='__main__':
-    #img = geotiff.imread(R"C:\Users\echo\Code\unet5\weights\Squatter\TS\20191018SA1_B05_3SW23C.tif")
-    #img = np.nan_to_num(img)
-    #img = cv2.resize(img, None, fx=0.051,fy=0.051)
-    #cv2.imshow('',img[:,:,1:4]/256)
-    #cv2.imshow('xxx',img[:,:,4]/256)
-    #cv2.waitKey(0)
-    #print(np.isnan(img).sum())
-    #quit()
 
-    ray.init(num_cpus=4)
-    t0=time.time()
-    xxx(R"C:\Users\echo\Code\unet5\weights\Squatter\TS", 
-            'Squatter', 'a')
-    t1=time.time()
-    print('total time:  %.1f'%(t1-t0))
+def calculate_accuracy(fname_json, fname_pred, out_txt_name):
+
+    fid = open(out_txt_name, 'a')
+    
+    gt = illustrate_mask_bw(fname_json)
+    pred = cv2.imread(fname_pred, 2)
+    mask = 255 * np.ones_like(pred)
+    idx = np.where(gt==128)
+    mask[idx] = 0    
+    assert pred.shape == gt.shape
+    TP = np.bitwise_and(pred, gt)
+    TP = np.bitwise_and(TP, mask)
+    nTP = np.count_nonzero(TP)
+
+    FP = np.bitwise_and( pred, np.bitwise_not(gt) )
+    FP = np.bitwise_and(FP, mask)
+
+    nFP = np.count_nonzero(FP)
+
+    gt = np.bitwise_and(gt, mask)
+    nGT = np.count_nonzero(gt)
+
+    print(os.path.split(fname_json)[-1])
+    fid.write(os.path.split(fname_json)[-1])
+    fid.write('\n')
+
+    # How many selected items are relevant
+    precis = 100*nTP/(nTP + nFP)
+    print('Precision: %.1f%%'%precis)
+    fid.write('Precision: %.1f%%\n'%precis)
+
+    # How many relevant items are selected
+    recall = 100*nTP/nGT
+    print('Recall   : %.1f%%'%recall)
+    fid.write('Recall   : %.1f%%\n'%recall)
+    fid.write('\n')
+    fid.close()
+
+    return precis, recall
+
+
+if __name__=='__main__':
+    inp_dir = R'C:\Users\dva\unet_dsm\weights\Squatter\TS-1'
+    t0 = time.time()
+    xxx(inp_dir, 'Squatter', 'a')
+    t1 = time.time()
+    print('Total time:  %.0f'%(t1-t0))
     quit()
 
-    dir = '/mnt/crucial-ssd/home/insight/Pictures/lands_1963_house_ts'
-    ftypes = ['*.JPG', '*.jpg', '*tif']
-    files = []
-    for ftype in ftypes:
-        files.extend(glob.glob(dir+'/'+ftype))
-
-    for f in files:
-        print(f)
-        illustrate_mask(f,cfg.classes_dict)
-    quit(0)
-
-
-    inp='/home/on99/Downloads/IMG_3588.jpg'
-    out_dir=os.path.join(os.path.split(inp)[0] , 'slice')
-    try:
-        os.mkdir(out_dir)
-    except:
-        pass
-
-    im_folder = os.path.join(out_dir,'train')
-    ann_folder = os.path.join(out_dir,'annotation')
-    try:
-        os.mkdir(ann_folder)
-    except:
-        pass
-    try:
-        os.mkdir(im_folder)
-    except:
-        pass
 
